447686aa8d5e8a89b45b6edc5d423e7513c45c8b
Roger Ferreira Da Silva <74381457+fsRoger@users.noreply.github.com>
2024-01-23 09:29:53 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Skills from "./components/Skills/Skills";
 import Experience from "./components/Experience/Experience";
-import Projects from "./components/Projects/projects";
+import Projects from "./components/Projects/Projects";
 import Depositions from "./components/Depositions/Depositions";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
